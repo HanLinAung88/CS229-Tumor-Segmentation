0b3bd063dc7da4d70758f806d1087252c5105f8b
Sajana Hemandra Weerawardhena <sajana@stanford.edu>
2018-11-24 19:16:33 -0800

    tensorboard added

--- get_data.py
+++ get_data.py
@@ -2,3 +2,10 @@ import requests
 url = 'http://peipa.essex.ac.uk/pix/mias/all-mias.tar.gz'
 r = requests.get(url, allow_redirects=True)
 open('all-mias.tar.gz','wb').write(r.content)
+
+#folow this with in the shell:
+# sudo apt-get update
+# sudo apt-get p7zip-full
+# 7z e all-mias.tar.gz  && 7z x all-mias.tar
+# rm all-mias.tar.gz
+# rm all-mias.tar
